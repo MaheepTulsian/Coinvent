e36cd61c1b27a0855020d8ccec183255d0a31af8
singhsatyendra8052 <singhsatyendra8052@gmail.com>
2024-03-20 23:21:48 +0530

    updated the organization schema

--- backend/routes/organisationRoute.js
+++ backend/routes/organisationRoute.js
@@ -27,57 +27,69 @@ const getUserClaims = (req, res) => {
 };
 
 //to create a new event
-organisationRoute.post(
-  "/createEvent/:organisation_name",
-  upload.none(),
-  async (req, res) => {
-    const event = new Event({
-      event_title: req.body.event_title,
-      event_date: req.body.event_date,
-      no_of_days: req.body.no_of_days,
-      event_description: req.body.event_description,
-      event_venue: req.body.event_venue,
-      rule_regulation: req.body.rule_regulation,
-      eligibility: req.body.eligibility,
-      cover_img_url: req.body.cover_img_url,
-      nft_img_url: req.body.nft_img_url,
-      max_tickets_available: req.body.max_tickets_available,
-      ticket_price: req.body.ticket_price,
-      ticket_sold: 0,
-      ticket_available: req.body.max_tickets_available,
-      max_ticket_per_person: req.body.max_ticket_per_person,
-      wallet_address: req.body.wallet_address,
+organisationRoute.post("/createEvent", upload.none(), async (req, res) => {
+  const event = new Event({
+    event_title: req.body.event_title,
+    event_date: req.body.event_date,
+    no_of_days: req.body.no_of_days,
+    event_description: req.body.event_description,
+    event_venue: req.body.event_venue,
+    rule_regulation: req.body.rule_regulation,
+    eligibility: req.body.eligibility,
+    cover_img_url: req.body.cover_img_url,
+    nft_img_url: req.body.nft_img_url,
+    max_tickets_available: req.body.max_tickets_available,
+    ticket_price: req.body.ticket_price,
+    ticket_sold: 0,
+    ticket_available: req.body.max_tickets_available,
+    max_ticket_per_person: req.body.max_ticket_per_person,
+    wallet_address: req.body.wallet_address,
+  });
+  // const user = getUserClaims();
+
+  try {
+    const claims = getUserClaims(req, res);
+
+    // Check if claims exist
+    if (!claims) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+    // const cookie = req.cookies["jwt"];
+
+    // const claims = jwt.verify(cookie, process.env.ACCESS_TOKEN_SECRET);
+    const savedEvent = await event.save();
+
+    // Find the organization by its name
+    const organisation = await Organisation.findOne({
+      _id: claims._id,
     });
 
-    try {
-      const savedEvent = await event.save();
-
-      // Find the organization by its name
-      const organisation = await Organisation.findOne({
-        organisation_name: req.params.organisation_name,
-      });
-
-      // Ensure organisation is found
-      if (!organisation) {
-        return res.status(404).json({ message: "Organization not found" });
-      }
-
-      // Associate the event with the organization by pushing its ID into the events array
-      organisation.events.push(savedEvent._id);
-      await organisation.save(); // Save the updated organisation
-
-      res.json(savedEvent);
-    } catch (error) {
-      res.status(500).json({ message: error.message });
+    // Ensure organisation is found
+    if (!organisation) {
+      return res.status(404).json({ message: "Organization not found" });
     }
+
+    // Associate the event with the organization by pushing its ID into the events array
+    organisation.events.push(savedEvent._id);
+    await organisation.save(); // Save the updated organisation
+
+    res.json(savedEvent);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
-);
+});
 
 //to update the event details
-organisationRoute.patch("/updateEvent/:id", async (req, res) => {
+organisationRoute.patch("/updateEvent", async (req, res) => {
   try {
+    const claims = getUserClaims(req, res);
+
+    // Check if claims exist
+    if (!claims) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
     const updatedEvent = await Event.updateOne(
-      { _id: req.params.id },
+      { _id: claims._id },
       {
         $set: {
           event_title: req.body.event_title,
@@ -103,10 +115,16 @@ organisationRoute.patch("/updateEvent/:id", async (req, res) => {
 });
 
 //to show all the events of an organisation
-organisationRoute.get("/showEvents/:organisation_name", async (req, res) => {
+organisationRoute.get("/showEvents", async (req, res) => {
   try {
+    const claims = getUserClaims(req, res);
+
+    // Check if claims exist
+    if (!claims) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
     const organisation = await Organisation.findOne({
-      organisation_name: req.params.organisation_name,
+      _id: claims._id,
     });
     if (!organisation) {
       return res.status(404).json({ message: "Organization events not found" });
@@ -193,7 +211,9 @@ organisationRoute.get("/user2", async (req, res) => {
       });
     }
 
-    const user = await Organisation.findOne({ _id: claims._id });
+    const user = await Organisation.findOne({ _id: claims._id }).populate(
+      "events"
+    );
 
     // const { ...data } = await user.toJSON();
     res.send(user);
